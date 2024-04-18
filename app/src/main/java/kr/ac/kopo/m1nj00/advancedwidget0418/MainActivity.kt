e0d81fa7d9a92d6467e7813fb4c9eb2ab8d4d55d
kimprosminju <b.m1nj00371@gmail.com>
2024-04-18 15:00:53 +0900

    Advanced Widget

---
+++ app/src/main/java/kr/ac/kopo/m1nj00/advancedwidget0418/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.kopo.m1nj00.advancedwidget0418
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_date)
+    }
+}
