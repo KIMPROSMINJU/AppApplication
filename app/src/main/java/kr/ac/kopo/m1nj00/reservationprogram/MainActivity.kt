3ef3bf24b4f5450a6589a81a753713c71d2015b7
kimprosminju <b.m1nj00371@gmail.com>
2024-04-25 14:23:31 +0900

    reservation program_self made

--- app/src/main/java/kr/ac/kopo/m1nj00/reservationprogram/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/m1nj00/reservationprogram/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.ac.kopo.m1nj00.reservationprogram
 
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
@@ -8,32 +9,31 @@ import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.DatePicker
 import android.widget.RadioGroup
 import android.widget.RadioGroup.OnCheckedChangeListener
 import android.widget.TextView
 import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import java.util.Calendar
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     lateinit var chrono : Chronometer
-    lateinit var btnStart : Button
-    lateinit var btnDone : Button
     lateinit var rg : RadioGroup
-    lateinit var calendar : CalendarView
+    lateinit var calendar : DatePicker
     lateinit var timePick : TimePicker
     lateinit var textResult : TextView
     var selectedYear : Int = 0
     var selectedMonth : Int = 0
     var selectedDay : Int = 0
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         chrono = findViewById(R.id.chrono)
-        btnStart = findViewById(R.id.btnStart)
-        btnDone = findViewById(R.id.btnDone)
         rg = findViewById(R.id.rg)
         calendar = findViewById(R.id.calendar)
         timePick = findViewById(R.id.timePick)
@@ -43,24 +43,25 @@ class MainActivity : AppCompatActivity() {
         timePick.visibility = View.INVISIBLE
 
         rg.setOnCheckedChangeListener(rgListner)
-        btnStart.setOnClickListener{
+        chrono.setOnClickListener{
             chrono.base = SystemClock.elapsedRealtime()
             chrono.start()
             chrono.setTextColor(Color.MAGENTA)
         }
 
-        btnDone.setOnClickListener{
+        textResult.setOnClickListener{
             chrono.stop()
             chrono.setTextColor(Color.CYAN)
             textResult.setText("" + selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일")
             textResult.append(" " + timePick.currentHour + "시 ")
-            textResult.append("" + timePick.currentMinute + "분")
+            textResult.append("" + timePick.currentMinute + "분 예약됨")
         }
-        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+        calendar.setOnDateChangedListener { view, year, month, dayOfMonth ->
             selectedYear = year
-            selectedMonth = month
+            selectedMonth = month + 1 // 월은 0부터 시작하므로 1을 더해줌
             selectedDay = dayOfMonth
         }
+
     }
     var rgListner = OnCheckedChangeListener{group, checkedId->
         calendar.visibility = View.INVISIBLE
